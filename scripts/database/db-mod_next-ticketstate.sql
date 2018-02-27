57734908843b8935449ce4fe95222317b3ba4baa
derco0n <dennis_marx@web.de>
2018-02-27 15:26:35 +0100

    Delete db-mod_next-ticketstate.sql

--- scripts/database/db-mod_next-ticketstate.sql
+++
@@ -1,6 +0,0 @@
-USE otrs;
-
-ALTER TABLE `standard_template`
-	ADD COLUMN `fk_tickstateIDnextDefault` SMALLINT NOT NULL DEFAULT '4' AFTER `change_by`; ##Default 4=open
-ALTER TABLE `standard_template`
-	ADD CONSTRAINT `FK_ticket_state_id` FOREIGN KEY (`fk_tickstateIDnextDefault`) REFERENCES `ticket_state` (`id`);
